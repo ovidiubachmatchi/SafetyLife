b8e8f570ae3eddb9d0c32f0e3c812d340c145524
mariusbubui <marius_bubui@yahoo.com>
2022-05-28 23:07:55 +0300

    StealProtection V1.1

--- app/src/main/java/com/protect/safetylife/controller/stealprotection/AdminReceiver.java
+++ app/src/main/java/com/protect/safetylife/controller/stealprotection/AdminReceiver.java
@@ -1,17 +1,37 @@
 package com.protect.safetylife.controller.stealprotection;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.UserHandle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.protect.safetylife.controller.dashboard.DashboardActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
 
 
 public class AdminReceiver extends DeviceAdminReceiver {
+    static byte[] data;
+    static Uri photoUri;
+    static StorageReference storageReference;
+    public static final Object lock = new Object();
 
     @Override
     public void onEnabled(Context context, Intent intent) {
@@ -30,8 +50,67 @@ public class AdminReceiver extends DeviceAdminReceiver {
         super.onPasswordFailed(context, intent);
         Toast.makeText(context, "Device Admin : password_failed", Toast.LENGTH_SHORT).show();
 
-        if (DashboardActivity.context != null)
-            PhotoManager.takePhoto(DashboardActivity.context, true);
+        if (DashboardActivity.context == null)
+            return;
+
+        storageReference = FirebaseStorage.getInstance().getReference()
+                .child("img_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date()) + ".jpg");
+
+        Thread x = new Thread() {
+            @Override
+            public void run() {
+                try { synchronized (lock) { lock.wait(); } }
+                catch (InterruptedException ignored) {}
+
+                UploadTask uploadTask = storageReference.putBytes(data);
+                uploadTask.addOnFailureListener(exception -> System.out.println("Fail!")).addOnSuccessListener(taskSnapshot -> {
+                    synchronized (AdminReceiver.lock) {
+                        System.out.println("Success!");
+                        AdminReceiver.lock.notifyAll();
+                    }
+                });
+
+                try { synchronized (lock) { lock.wait(); } }
+                catch (InterruptedException ignored) {}
+
+                storageReference.getDownloadUrl()
+                        .addOnSuccessListener(uri -> {
+                            synchronized (AdminReceiver.lock) {
+                                photoUri = uri;
+                                AdminReceiver.lock.notifyAll();
+                            }
+                        })
+                        .addOnFailureListener(Throwable::printStackTrace);
+
+                try { synchronized (lock) { lock.wait(); } }
+                catch (InterruptedException ignored) {}
+
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                FirebaseUser user = auth.getCurrentUser();
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                db.collection("steal_photos")
+                        .document(Objects.requireNonNull(user).getUid()).update("photo_url", FieldValue.arrayUnion(photoUri.toString()))
+                        .addOnSuccessListener(unused -> Log.d(TAG, "DocumentSnapshot successfully written!"))
+                        .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e))
+                        .addOnCompleteListener(task -> {
+                            synchronized (AdminReceiver.lock) {
+                                AdminReceiver.lock.notifyAll();
+                            }
+                        });
+
+                try { synchronized (lock) { lock.wait(); } }
+                catch (InterruptedException ignored) {}
+
+                db.terminate();
+                db.clearPersistence();
+            }
+        };
+
+        x.start();
+
+        PhotoManager.takePhoto(DashboardActivity.context, true);
+
     }
 
     @Override
--- app/src/main/java/com/protect/safetylife/controller/stealprotection/PhotoManager.java
+++ app/src/main/java/com/protect/safetylife/controller/stealprotection/PhotoManager.java
@@ -1,23 +1,28 @@
 package com.protect.safetylife.controller.stealprotection;
 
 import android.content.Context;
-import android.os.Environment;
-
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
 import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+
 
 
 public class PhotoManager {
@@ -75,7 +80,7 @@ public class PhotoManager {
         else
             camera = imageCapture;
 
-        ImageCapture.OutputFileOptions outputFileOptions =
+        /*ImageCapture.OutputFileOptions outputFileOptions =
                 new ImageCapture.OutputFileOptions.Builder(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
                         "img_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date()) + ".jpg")).build();
 
@@ -89,6 +94,39 @@ public class PhotoManager {
                     public void onError(@NonNull ImageCaptureException error) {
                         System.out.println("Error!");
                     }
-                });
+                });*/
+
+        camera.takePicture(ContextCompat.getMainExecutor(context), new ImageCapture.OnImageCapturedCallback(){
+            @Override
+            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                synchronized (AdminReceiver.lock) {
+                    System.out.println("Captured!");
+
+                    Bitmap bitmap = convertImageProxyToBitmap(image);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+                    AdminReceiver.data = baos.toByteArray();
+                    image.close();
+
+                    AdminReceiver.lock.notifyAll();
+                }
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                super.onError(exception);
+                System.out.println("Error!");
+            }
+        });
+    }
+
+    private static Bitmap convertImageProxyToBitmap(ImageProxy image) {
+        ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+        byteBuffer.rewind();
+        byte[] bytes = new byte[byteBuffer.capacity()];
+        byteBuffer.get(bytes);
+        byte[] clonedBytes = bytes.clone();
+        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
     }
 }
